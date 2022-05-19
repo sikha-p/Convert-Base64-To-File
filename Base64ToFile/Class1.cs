1ddde752b2347cc1a3b93f6484869b06d22a80ff
Sikha <84059776+sikha-p@users.noreply.github.com>
2022-05-19 18:41:07 +0530

    Add files via upload

---
+++ Base64ToFile/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+
+namespace Base64ToFile
+{
+    public class Class1
+    {
+        public static string convertBase64ToFile(string base64String, string outputFilePath)
+        {
+            try
+            {
+                byte[] bytes = Convert.FromBase64String(base64String);
+                File.WriteAllBytes(outputFilePath, bytes);
+                return "success";
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+    }
+}
